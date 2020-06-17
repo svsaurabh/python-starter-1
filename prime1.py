7314814a0d407f0466695a7cbc80f2256316e7bd
sv-shiansh <sv_shiansh@outlook.com>
2020-06-17 13:19:17 +0530

    Added an operation to sum all the Prime numbers in your Code

--- prime1.py
+++ prime1.py
@@ -1,5 +1,6 @@
 flag = 0
 count = 0
+sum = 0
 n = int(input("Enter the range: "))
 print("Prime Numbers: ")
 for i in range(2, n+1):
@@ -9,9 +10,10 @@ for i in range(2, n+1):
             break
     if flag == 0:
         print(i, "  ", end = "")
+        sum += i
         count += 1
     else:
         flag = 0
 print("")
 print("The total number of Prime Numbers in the given range are: ", count)
-
+print("Sum of all PRIME numbers: ", sum)
