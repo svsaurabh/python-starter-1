3bf697f9c746634f091fe3ce5544adf0de1a58f0
svshriyansh <prince.vishwakarma2000@gmail.com>
2020-10-04 20:03:51 +0530

    Km_to_miles

---
+++ Km_to_miles.py
@@ -0,0 +1,6 @@
+#the programe will convert the km to miles
+kilometer = float(input('Enter the number in km '))
+convert_fom = 0.621371
+#calculate the mile value
+miles = kilometer * convert_fom
+print('{} Km is equal to {} miles'.format(kilometer,miles))
